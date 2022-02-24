f36da7da3273d29b576da8270357e7a8935081a4
Narayanan s <73871484+ramsrii@users.noreply.github.com>
2022-02-24 20:41:43 +0530

    Added some change

--- Basic Java/src/Assign13.java
+++ Basic Java/src/Assign13.java
@@ -63,7 +63,7 @@ public class Assign13 {
         }
         else
         {
-            System.out.println("Invalid array size");
+            System.out.println("Invalid array size summa");
         }
     }
 }
